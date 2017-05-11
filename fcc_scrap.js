6db41634527e1e69cdd4e7a2bd6425821ae893de
mccarvik <mccarviks@gmail.com>
2017-05-11 20:24:33 +0000

    fx

--- fcc_scrap.js
+++ fcc_scrap.js
@@ -59,4 +59,26 @@ function strictVsRegEquality() {
   console.log(obj === null); // false
   console.log(obj === undefined); // false
 }
-strictVsRegEquality()
+// strictVsRegEquality()
+
+// Switch statement, syntax example
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+  }  
+  return answer;  
+}
+console.log(caseInSwitch(1));
