4db42ac51c3f66133f6610cae22b9b00df1970e0
mccarvik <mccarviks@gmail.com>
2017-05-11 19:32:15 +0000

    small change

--- FCC_scrap.js
+++ FCC_scrap.js
@@ -4,4 +4,25 @@ function reusuableFunction() {
     console.log("Hi World");
 }
 
-reusuableFunction();
+// Variables which are used without the var keyword are automatically created 
+// in the global scope. You should always declare your variables with var.
+var myGlobal = 10;
+
+function fun1() {
+  oopsGlobal = 5;
+}
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+fun1();
+fun2();
+// reusuableFunction();
