25543dd6ee36a89add9ed37865bd5878db832810
mccarvik <mccarviks@gmail.com>
2017-05-13 15:20:58 +0000

    algo update

--- javascript/freeCodeCamp/algos.js
+++ javascript/freeCodeCamp/algos.js
@@ -6,5 +6,38 @@ function reverseString(str) {
   }
   return rev.join("")
 }
-console.log(reverseString("hello"));
+// console.log(reverseString("hello"));
+
+function factorialize(num) {
+  var prod=1;
+  for (var i=1; i<=num; i++) {
+    prod *= i;
+  }
+  
+  return prod;
+}
+// console.log(factorialize(5));
+
+
+
+function palindrome(str) {
+  // format string
+  var expression = /[a-zA-Z0-9]+/g;
+  str = str.match(expression).join("");
+  expression = /\S+/g;
+  // console.log(str.match(expression));
+  str = str.match(expression).join("");
+  var arr = str.split("");
+  var len = (Math.floor(arr.length / 2));
+  for (var i=0; i < len; i++) {
+    if (arr.pop().toUpperCase() == arr.shift().toUpperCase()) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+var s = "not a palindrome";
+// console.log(palindrome(s));
 
