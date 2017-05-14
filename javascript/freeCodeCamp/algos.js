b153b507aea3905c37350da82bf6d2c75d03f3b3
mccarvik <mccarviks@gmail.com>
2017-05-14 02:09:56 +0000

    updated chart

--- javascript/freeCodeCamp/algos.js
+++ javascript/freeCodeCamp/algos.js
@@ -67,7 +67,7 @@ function titleCase(str) {
   }
   return ret.join(" ");
 }
-console.log(titleCase("I'm a little tea pot"));
+// console.log(titleCase("I'm a little tea pot"));
 
 function largestOfFour(arr) {
   var ret = [];
@@ -82,6 +82,104 @@ function largestOfFour(arr) {
   }
   return ret;
 }
-
 // console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);)
 
+function confirmEnding(str, target) {
+  slice = str.slice(str.length - target.length, str.length);
+  return (slice == target);
+}
+// console.log(confirmEnding("Open sesame", "same"));
+
+function repeatStringNumTimes(str, num) {
+  var ret = "";
+  if (num < 1) {
+    return ret;
+  } else {
+    for (var i=0; i < num; i++) {
+      ret += str;
+    }
+  }
+  return ret;
+}
+// console.log(repeatStringNumTimes("abc", 3));
+
+function chunkArrayInGroups(arr, size) {
+  var ret = [];
+  while (arr.length > 0) {
+    var t_arr = [];
+    for (var i=0; i<size; i++){
+      if (arr.length === 0) { break; }
+      t_arr.push(arr.shift());
+    }
+    ret.push(t_arr);
+  }
+  return ret;
+}
+// console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+
+function slasher(arr, howMany) {
+  return arr.slice(howMany,arr.length);
+}
+// console.log(slasher([1, 2, 3], 2));
+
+function mutation(arr) {
+  var spl = arr[0].toUpperCase();
+  var check = arr[1].split("");
+  for (var i=0; i < check.length; i++){
+    if (spl.indexOf(check[i].toUpperCase()) < 0) { return false; }
+  }
+  return true;
+}
+// console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]));
+
+
+function bouncer(arr) {
+  return arr.filter(function(s) {
+    if(s) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+};
+// console.log(bouncer([7, "ate", "", false, 9]));
+
+function destroyer(arr) {
+  ret = arguments[0];
+  for (var i=1; i<arguments.length; i++) {
+    while (ret.indexOf(arguments[i]) >= 0) {
+      ret.splice(ret.indexOf(arguments[i]), 1);
+    }
+  }
+  return ret;
+}
+// console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+
+function getIndexToIns(arr, num) {
+  arr.push(num);
+  arr = arr.sort(function(a,b) { return a-b; });
+  return arr.indexOf(num);
+}
+// console.log(getIndexToIns([3, 10, 5], 50));
+
+function rot13(str) { // LBH QVQ VG!
+  var ret = [];
+  var spl = str.split("");
+  for (var i=0; i < spl.length; i++) {
+    if (isLetter(spl[i])) {
+      if (spl[i].charCodeAt(0) < 78) {
+        ret.push(String.fromCharCode(spl[i].charCodeAt(0) + 13));
+      } else {
+        ret.push(String.fromCharCode(spl[i].charCodeAt(0) - 13));
+      }
+    } else {
+      ret.push(spl[i]);
+    }
+  }
+  return ret.join("");
+}
+
+function isLetter(str) {
+  return str.match(/[a-z]/i);
+}
+console.log(rot13("SERR PBQR PNZC"));
