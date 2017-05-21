c21e1e92139152517c5aaaef6b1e18bde8c975a1
mccarvik <mccarviks@gmail.com>
2017-05-21 23:53:00 +0000

    algos

--- javascript/freeCodeCamp/int_algos.js
+++ javascript/freeCodeCamp/int_algos.js
@@ -9,3 +9,23 @@ function sumAll(arr) {
   return sum;
 }
 // console.log(sumAll([1, 4]));
+
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  both = [];
+  for (var i=0; i < arr1.length; i++) {
+    for (var k=0; k < arr2.length; k++) {
+      if (arr1[i] === arr2[k]) {
+        both.push(arr1[i]);
+        break;
+      }
+    }
+  }
+  for (var b=0; b < both.length; b++) {
+    arr1.splice(arr1.indexOf(both[b]), 1);
+    arr2.splice(arr2.indexOf(both[b]), 1);
+  }
+  return arr1.concat(arr2);
+}
+// console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
