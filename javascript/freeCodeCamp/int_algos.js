d4c407e0baa00dae0c3669e4bf6dc1ab660b6a9a
mccarvik <mccarviks@gmail.com>
2017-05-24 20:15:08 +0000

    small udpate

--- javascript/freeCodeCamp/int_algos.js
+++ javascript/freeCodeCamp/int_algos.js
@@ -29,3 +29,39 @@ function diffArray(arr1, arr2) {
 }
 // console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
 
+function fearNotLetter(str) {
+  var first = str.charCodeAt(0);
+  for (var i=1; i < str.length; i++) {
+    if (str.charCodeAt(i) != first + i) {
+      return String.fromCharCode(first+i);
+    }
+  }
+  return undefined;
+}
+// console.log(fearNotLetter("abcdefghjklmno"));
+
+function pairElement(str) {
+  var arr = str.split("");
+  var ret = [];
+  for (var i=0; i<arr.length; i++) {
+    var temp = "";
+    switch (arr[i]) {
+      case "G":
+        temp = "C";
+        break;
+      case "C":
+        temp = "G";
+        break;
+      case "A":
+        temp = "T";
+        break;
+      case "T":
+        temp = "A";
+        break;
+    }
+    ret.push([arr[i],temp]);
+  }
+  return ret;
+}
+// console.log(pairElement("GCG"));
+
