73b7f7d0a39bf719699bca29613789fa92e4a43e
mccarvik <mccarviks@gmail.com>
2017-06-02 20:21:27 +0000

    need to figure out the routes setup for ng_routes

--- javascript/nodejs_angular/scripts/ng_routes.js
+++ javascript/nodejs_angular/scripts/ng_routes.js
@@ -1,9 +1,25 @@
 angular.module('app', ['ngRoute'])
+
+  .controller('TodoController', ['$scope', function ($scope) {
+    console.log('here1');
+    $scope.todos = [
+      { name: 'AngularJS Directives', completed: true },
+      { name: 'Data binding', completed: true },
+      { name: '$scope', completed: true },
+      { name: 'Controllers and Modules', completed: true },
+      { name: 'Templates and routes', completed: true },
+      { name: 'Filters and Services', completed: false },
+      { name: 'Get started with Node/ExpressJS', completed: false },
+      { name: 'Setup MongoDB database', completed: false },
+      { name: 'Be awesome!', completed: false },
+    ];
+  }])
+  
   .config(['$routeProvider', function ($routeProvider) {
-    console.log('here');
+    console.log('here2');
     $routeProvider
-      .when('/', {
-        templateUrl: 'todos.html',
+      .when('/todos', {
+        templateUrl: '/views/todos.html',
         controller: 'TodoController'
       });
   }]);
